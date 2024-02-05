de63b1508bfb1aec7d66a923e70c6f3cd16ccf65
Nirmal Bhuva <91771820+Nirmal-Bhuva@users.noreply.github.com>
2024-02-06 00:08:42 +0530

    Create server.js

---
+++ testing_api/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const books = [
+    {id: 1, title: 'Alice in Wonderland', author: 'Lewis Carrol'},
+    {id: 2, title: 'Around the World in eighty days', author: 'Jules Verne'},
+    {id: 3, title: 'Utopia', author: 'Sir Thomas Moor'},
+   ]
+   
+   app.get('/api/books',function(req, res) {
+    res.json(books)
+   })
+
+app.get('/', (req, res) => {
+ res.send('Hello World!')
+})
+app.listen(port, () => {
+ console.log(`Server running at http://localhost:${port}`)
+})
